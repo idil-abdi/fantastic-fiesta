b33a2b80b45602b0b92ef9e121eb13ec81cb9e60
Edil Abdiaziz <edilabdiaziz@gmail.com>
2023-09-07 17:14:11 +0100

    upddate JavaScript Project

--- toDoList/todolist.js
+++ toDoList/todolist.js
@@ -11,21 +11,18 @@ renderTodoList();
 function renderTodoList() {
     let todoListHTML = '';
 
-    for(let i = 0; i < todoList.length; i++) {
-        const todoObject = todoList[i];
-        //const name = todoObject.name;
-        //const dueDate = todoObject.dueDate;
+    todoList.forEach(function(todoObject, indext) {
         const {name, dueDate} = todoObject
         const html = `
             <div>${name}</div>
             <div>${dueDate}</div>
             <button onclick="
-                todoList.splice(${i}, 1);
+                todoList.splice(${indext}, 1);
                 renderTodoList();
             " class="delete-to-button">Delete</button>
             `;
         todoListHTML += html
-    }
+    })
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML
 }
